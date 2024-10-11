a096a56f6bd05843d023743ea68103f6a0197f0b
Thomas Fox <47598118+tomfox747@users.noreply.github.com>
2024-10-11 16:33:58 +0100

    fix Service injection bug

--- Tech-Test/Program.cs
+++ Tech-Test/Program.cs
@@ -2,14 +2,13 @@ using Tech_Test.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
+builder.Services.AddScoped<IWeatherForecastService, WeatherForecastService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddScoped<IWeatherForecastService, WeatherForecastService>();
 
 var app = builder.Build();
 
